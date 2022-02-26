6845101544db08421f4436abbe9b65279d354101
novahi <nguoidungemail1@gmail.com>
2022-02-26 09:33:51 +0000

    hehe

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -23,7 +23,11 @@ class UserController {
     })
     const isLogin = await  page.evaluate(() => window.location.href)
     if(isLogin.includes(linkLogin)) {
-      await page.waitForSelector("input[name='username']")
+      await Peomise.all([
+         page.waitForSelector("input[name='username']"),
+         page.waitForSelector("input[name='password']"),
+         page.waitForSelector("button[type='submit']")
+    ])
       await page.type("input[name='username']", process.env.USERNAME_IG, { delay: 200});
       await page.type("input[name='password']", process.env.PASSWORD_IG, { delay: 200 });
       await Promise.all([page.click("button[type='submit']"), page.waitForNavigation({ waitUntil: "networkidle0"})])
