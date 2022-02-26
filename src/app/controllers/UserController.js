df2ec18625945b17822d4420489ee335a2203a11
novahi <53039974+novahi@users.noreply.github.com>
2022-02-26 14:26:09 +0000

    Update UserController.js

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -10,7 +10,7 @@ class UserController {
     let { url } = req.body
     url = url.toLowerCase()
     const browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       args: [
              '--no-sandbox',
              '--disable-setuid-sandbox'
